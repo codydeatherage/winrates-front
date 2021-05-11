b5fc5e175b679bcd570875edc559b8c59be6f30e
codydeatherage <cdeatherage14@gmail.com>
2021-05-10 22:10:17 -0700

    working with datadragon :)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
 import './App.css';
+import ChampionsStats from './components/ChampionStats'
+import axios from 'axios';
+import { useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
+const App = () => {
+  const [champList, setChampList] = useState('');
+
+  useEffect(() => {
+    if (!champList) {
+      getChampData();
+    }
+  }, []);
+
+  const getChampData = async () => {
+    const fetch = await axios.get('http://ddragon.leagueoflegends.com/cdn/11.9.1/data/en_US/champion.json')
+      .then(async (resp) => {
+        let champ = await resp.data;
+        let allChamps = [];
+        for (let ch in champ.data) {
+          allChamps.push(ch);
+        }
+        setChampList(allChamps);
+      })
+  }
+
+  if (champList) {
+    return (
+      <div className="App">
+        <div className="division-header">
+          Challenger
+          </div>
+        <div className="champions-section">
+          {champList.map((name, index) => <ChampionsStats name={name} key={index}></ChampionsStats>)}
+        </div>
+
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className="App">
         <div className="division-header">
           Challenger
         </div>
         <div className="champions-section">
-          <div className="info"></div>
-          <div className="info"></div>
-          <div className="info"></div>
-          <div className="info"></div>
-          <div className="info"></div>
-          <div className="info"></div>
-          <div className="info"></div>
-          <div className="info"></div>
-          <div className="info"></div>
-          <div className="info"></div>
-          <div className="info"></div>
+          <div>Loading...</div>
         </div>
-
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ChampionStats.js
@@ -0,0 +1,7 @@
+const ChampionsStats = ({name}) =>{
+    return(
+        <div className="info">{name}</div>
+    )
+}
+
+export default ChampionsStats;
